f0e9a0f565e957f8c5652029e99f313a61cc0232
ErrorDeleted <maldach1@student.cvtc.edu>
2023-02-13 00:21:36 -0600

    Emergency Save: Before installing npm server

---
+++ server.js
@@ -0,0 +1,25 @@
+require('dotenv').config() // Read variables from .env
+const exp = require('constants')
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5163
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .use(express.json)
+  .use(express.urlencoded({ extended: true }))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', function (req, res) { // Home is grabbed | Step 5 in Routes
+    res.render('pages/index')
+  })
+
+  .get('/health', function (req, res) { // Step 4 in Routes
+    res.status(200).send('Healthy')
+  })
+
+  .get('/about', function (req, res) { // Step 6 in Routes
+    res.render('pages/about')
+  })
+
+  .listen(PORT, () => console.log(`Listening on ${PORT}`))
