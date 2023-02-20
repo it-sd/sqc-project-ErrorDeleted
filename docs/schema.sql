3c1e23707646514d01efa0e42412a52bf3b70a2d
ErrorDeleted <maldach1@student.cvtc.edu>
2023-02-20 14:16:20 -0600

    Database Crated + Jasmine Added

---
+++ docs/schema.sql
@@ -0,0 +1,36 @@
+DROP TABLE WHEN EXISTS category;
+DROP TABLE WHEN EXISTS items;
+DROP TABLE WHEN EXISTS cart;
+DROP TABLE WHEN EXISTS user;
+
+CREATE TABLE category (
+  categoryId SERIAL PRIMARY KEY,
+  title TEXT NOT NULL,
+);
+
+CREATE TABLE items (
+  itemId SERIAL PRIMARY KEY,
+  categoryId INTEGER NOT NULL,
+  price INTEGER NOT NULL,
+  forSale INTEGER NOT NULL,
+  quanity INTEGER NOT NULL,
+); /*categoryId = FK*/
+
+CREATE TABLE cart (
+  cartId SERIAL PRIMARY KEY,
+  itemId INTEGER NOT NULL
+); /*itemId = FK*/
+
+CREATE TABLE user (
+  userId SERIAL PRIMARY KEY,
+  cartId INTEGER NOT NULL
+); /*cartId = FK*/
+
+INSERT INTO category (title)
+  VALUES
+ ('Warframe Part'),('Warframe Set'),('Weapon Part'),('Weapon Set'), 
+ ('Normal Mod'),('Archon Mod'),('Prime Mod'),('Un-veiled Riven'), 
+ ('Veiled Riven'), ('Lith Relic'), ('Neo Relic'), ('Meso Relic'), 
+ ('Axi Relic'), ('Requiem Relic')
+/* ^Only Table Fillable For Now^ */
+
